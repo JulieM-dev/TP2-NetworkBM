35ac0c4594226a97a79e657aea3a5c945d4fdf67
JulieM-dev <julie.neko.egirl@gmail.com>
2021-09-30 18:55:49 +0200

    Ajout objet form view

--- app/src/main/java/com/example/networkbm/AjoutObjetForm.kt
+++ app/src/main/java/com/example/networkbm/AjoutObjetForm.kt
@@ -2,14 +2,27 @@ package com.example.networkbm
 
 import android.app.AlertDialog
 import android.content.Context
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 
-class AjoutObjetForm (context: Context){
+class AjoutObjetForm (context: Context) : AppCompatActivity() {
 
-    var dialogBuilder : AlertDialog.Builder = AlertDialog.Builder(context)
+    val dialogBuilder : AlertDialog.Builder = AlertDialog.Builder(context)
     lateinit var alertDialog : AlertDialog
-    lateinit var editText : EditText
-    lateinit var button: Button
+    val editTextNom : EditText = findViewById(R.id.editTextNom)
+    val spinnerCouleur : Spinner  = findViewById(R.id.spinnerCouleur)
+    val buttonValider : Button = findViewById(R.id.buttonValider)
+    val buttonAnnuler : Button = findViewById(R.id.buttonAnnuler)
+
+    fun afficherFormulaire()
+    {
+        val formulaire : View = layoutInflater.inflate(R.layout.ajout_objet_form, null)
+
+
+    }
+
 
 }
